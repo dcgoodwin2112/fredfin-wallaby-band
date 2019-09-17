445ca9a397104fb28bdafa72073b03c9709bc0d5
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2019-09-16 23:26:16 -0400

    Add favicon images.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,24 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "FredfinWallaby",
+        short_name: "fredfin",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "content/assets/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
   // Customize your site metadata:
   siteMetadata: {
@@ -26,7 +44,7 @@ module.exports = {
       },
       {
         name: `Spotify`,
-        url:`https://open.spotify.com/artist/2V6ttGXnp9ZNMHRe3VqNB3`
+        url: `https://open.spotify.com/artist/2V6ttGXnp9ZNMHRe3VqNB3`,
       },
     ],
   },
