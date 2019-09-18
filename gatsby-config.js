6888d27fc3af51686376a987e2e5786ba55c9791
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2019-09-17 23:20:22 -0400

    Add gatsby-plugin-manifest and favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,24 +5,18 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: "FredfinWallaby",
-    //     short_name: "fredfin",
-    //     start_url: "/",
-    //     background_color: "#6b37bf",
-    //     theme_color: "#6b37bf",
-    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-    //     display: "standalone",
-    //     icon: "content/assets/icon.png", // This path is relative to the root of the site.
-    //     // An optional attribute which provides support for CORS check.
-    //     // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-    //     // Any invalid keyword or empty string defaults to `anonymous`
-    //     crossOrigin: `use-credentials`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `FrefinWallaby`,
+        short_name: `FW`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `content/assets/icon.png`,
+      },
+    },
   ],
   // Customize your site metadata:
   siteMetadata: {
